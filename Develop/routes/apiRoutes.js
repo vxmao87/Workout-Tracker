bcf365869fff5af46f622559ed82fc7bc99ea57b
Vinh Mao <vinhxmao87@gmail.com>
2020-07-02 22:21:44 -0700

    API routes initiated, server created, workout model initiated

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+
+// Route for creating a workout
+router.post("/api/workouts", (req, res) => {
+
+});
+
+
+// Route for getting all workouts
+router.get("/api/workouts", (req, res) => {
+
+});
+
+// Route for grabbing workouts within a certain range
+router.get("/api/workouts/range", (req, res) => {
+
+});
+
+// Route for updating a workout by its ID
+router.put("/api/workouts/:id", (req, res) => {
+
+})
+
+module.exports = router;
