e349a82bd216d703a0efc6eefbe5ca5295257b8a
Vinh Mao <vinhxmao87@gmail.com>
2020-07-10 16:25:43 -0700

    all API routes finished

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,23 +1,44 @@
 const router = require("express").Router();
+const db = require("../models");
 
 // Route for creating a workout
-router.post("/api/workouts", (req, res) => {
-
+router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body).then(dbWorkout => {
+        res.send(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
 });
 
 // Route for getting all workouts
 router.get("/api/workouts", (req, res) => {
-
+    db.Workout.find().sort({ day: -1 }).limit(1)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        })
 });
 
 // Route for grabbing workouts within a certain range
 router.get("/api/workouts/range", (req, res) => {
-
+    db.Workout.find({}).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    })
 });
 
 // Route for updating a workout by its ID
 router.put("/api/workouts/:id", (req, res) => {
-
+    db.Workout.findByIdAndUpdate(
+        req.params.id,
+        { $push: { exercises: req.body }},
+    ).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
 })
 
 module.exports = router;
